e6f90576ef8ee1c9eb073c14510ba4f5968ae40f
Graham Reeves <graham@grahamreeves.com>
2014-12-23 02:24:45 +0000

    Added demo unity project that uses PopUnity bundle

---
+++ Unity/Assets/Debug.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.InteropServices;
+
+public class Debug : MonoBehaviour {
+
+
+	[DllImport ("PopUnity")]
+	private static extern UInt64 Test();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		string Text = "Hello ";
+		Text += Test ();
+
+		Rect rect = new Rect (0, 0, Screen.width, Screen.height );
+
+		GUI.Label (rect, Text);
+	}
+}
