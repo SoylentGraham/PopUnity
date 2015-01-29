06ef983f7f8127b5dea4f605b4abdc9f6991b0c5
Graham Reeves <graham@grahamreeves.com>
2015-01-29 13:56:25 +0000

    Added some more test-if-c++11 is working code

--- src/Android.cpp
+++ src/Android.cpp
@@ -1,4 +1,17 @@
+#include <vector>
+#include <memory>
 
+template<typename T>
+using ofPtr = std::shared_ptr<T>;
+
+void Somethi8ng()
+{
+	int x = 124;
+	auto y = x;
+	
+	
+
+}
 
 extern "C" bool Test()
 {
